01c96b9b9aafce8cfa0876f7150393968a20ee2d
Eugene-prog <ieugene.prog@gmail.com>
2023-07-09 22:07:56 +0700

    feat: feat init structure with React Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import Login from "./routes/login";
+import Register from "./routes/register";
+import User from "./routes/user";
 import App from './App';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="login"/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/user",
+    element: <User/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
